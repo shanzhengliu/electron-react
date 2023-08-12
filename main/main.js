aea67a46b4e1983c786179438bddce69d2123e81
Shanzheng Liu <liushanzheng960522@outlook.com>
2023-08-12 16:44:15 +1000

    electron-react-framework

---
+++ main/main.js
@@ -0,0 +1,23 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev');
+
+function createWindow () {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+    const releasePath = isDev? "http://localhost:3000/" : `file://${path.resolve(__dirname, '../build/index.html')}`;
+    mainWindow.loadURL(releasePath);
+//   mainWindow.webContents.openDevTools()
+}
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
